156e3e8d45712c48547206ab14d03fef1907a4bb
oversparkling <nicholasongck@gmail.com>
2021-05-30 18:10:36 +0800

    Initialising XCode project

---
+++ Stanford CS193p course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford CS193p course
+//
+//  Created by Nicholas Ong on 30/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
